588903eaa8f15395b17a56692d79bf9e2bb5f791
Paul Peterson <pauljpeterson@gmail.com>
2023-10-27 13:20:39 -0700

    Fixed package.json error and removed leftover code

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
 import { Flex, Grid } from '@radix-ui/themes';
 
-export default async function Home() {
-  const open = await prisma.issue.count({
-    where: { status: 'OPEN' },
-  });
-  const inProgress = await prisma.issue.count({
-    where: { status: 'IN_PROGRESS' },
-  });
-  const closed = await prisma.issue.count({
-    where: { status: 'CLOSED' },
-  });
-
   return (
     <Grid columns={{ initial: '1', md: '2' }} gap="5">
       <Flex direction="column" gap="5">
@@ -18,4 +7,3 @@ export default async function Home() {
       </Flex>
     </Grid>
   );
-}
