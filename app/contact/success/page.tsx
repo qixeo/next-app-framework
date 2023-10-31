a94b7d634101c27e2ed533f3fef2c2cb68dc7026
Paul Peterson <pauljpeterson@gmail.com>
2023-10-30 17:12:39 -0700

    Contact form success page

--- app/contact/success/page.tsx
+++ app/contact/success/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Card, Heading } from '@radix-ui/themes';
 
 const EmailSuccessPage = () => {
-  return <div>Your email has been sent!</div>;
+  return (
+    <div className="flex items-center justify-center h-[calc(100vh-150px)]">
+      <Card size="5" className="shadow-lg">
+        <Heading weight="light">Your email has been sent!</Heading>
+      </Card>
+    </div>
+  );
 };
 
 export default EmailSuccessPage;
