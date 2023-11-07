e551cd5b3021c5028be6350b019fe0236d7642ca
Paul Peterson <pauljpeterson@gmail.com>
2023-11-07 11:45:09 -0800

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
+// The following line by itself will protect the whole site
 export { default } from 'next-auth/middleware';
 
+// Protect only what is listed in `matcher`
 export const config = {
-  matcher: ['/issues/new', '/issues/edit/:id+', '/users'],
+  matcher: ['/users/new', '/users/edit/:id+', '/users'],
 };
